252aa7c79fe3220a87a1168938083f7bf46b4d86
poonamjadhav98 <poonam.jadhav9298@gmail.com>
2022-04-01 10:29:12 +0530

    American guy commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -9,6 +9,16 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
+		
+		// test1
+		//test 2
+		
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		
+		
 
 	}
 
